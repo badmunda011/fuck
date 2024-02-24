978a1c8f8ba5d11053e06c1247c27af016a5902a
SUKH ❤️ <158456267+PBSUKH@users.noreply.github.com>
2024-02-24 20:44:32 +0530

    Update cbb.py

--- plugins/cbb.py
+++ plugins/cbb.py
@@ -17,7 +17,7 @@ async def cb_handler(client: Bot, query: CallbackQuery):
     data = query.data
     if data == "about":
         await query.message.edit_text(
-            text=f"<b>○ Creator : <a href="<b>○ ᴄʀᴇᴀᴛᴏʀ : <a href='tg://user?id={OWNER_ID}'>🕊️⃝‌ٖٖٖٖ ‌ٖٖٖٖٖ𝐁α∂ ❤️ᥫ᭡፝֟፝֟</a>\n○ ʟᴀɴɢᴜᴀɢᴇ : <code>Python3</code>\n○ sᴏᴜʀᴄᴇ ᴄᴏᴅᴇ : <a href='https://github.com/Badhacker98?tab=repositories'>Click here</a>\n○ sᴜᴘᴘᴏʀᴛ ɢʀᴏᴜᴘ : @ll_THE_BAD_BOT_ll</b>",",
+            text=f"<b>○ ᴄʀᴇᴀᴛᴏʀ : <a href='tg://user?id={OWNER_ID}'>🕊️⃝‌ٖٖٖٖ ‌ٖٖٖٖٖ𝐁α∂ ❤️ᥫ᭡፝֟፝֟</a>\n○ ʟᴀɴɢᴜᴀɢᴇ : <code>Python3</code>\n○ sᴏᴜʀᴄᴇ ᴄᴏᴅᴇ : <a href='https://github.com/Badhacker98?tab=repositories'>Click here</a>\n○ sᴜᴘᴘᴏʀᴛ ɢʀᴏᴜᴘ : @ll_THE_BAD_BOT_ll</b>",",
             disable_web_page_preview=True,
             reply_markup=InlineKeyboardMarkup(
                 [[InlineKeyboardButton("❌ ᴄʟᴏsᴇ ❌", callback_data="close")]]
