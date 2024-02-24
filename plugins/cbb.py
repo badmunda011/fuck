0a042b0843d2cb540da6c3becefdd835e9dc2fe7
SUKH ❤️ <158456267+PBSUKH@users.noreply.github.com>
2024-02-24 20:11:56 +0530

    Update cbb.py

--- plugins/cbb.py
+++ plugins/cbb.py
@@ -20,7 +20,7 @@ async def cb_handler(client: Bot, query: CallbackQuery):
             text=f"<b>○ Creator : <a href='tg://user?id={OWNER_ID}'>This Person</a>\n○ Language : <code>Python3</code>\n○ Library : <a href='https://docs.pyrogram.org/'>Pyrogram asyncio {__version__}</a>\n○ Source Code : <a href='https://github.com/CodeXBotz/File-Sharing-Bot'>Click here</a>\n○ Channel : @CodeXBotz\n○ Support Group : @CodeXBotzSupport</b>",
             disable_web_page_preview=True,
             reply_markup=InlineKeyboardMarkup(
-                [[InlineKeyboardButton("🔒 Close", callback_data="close")]]
+                [[InlineKeyboardButton("❌ ᴄʟᴏsᴇ ❌", callback_data="close")]]
             ),
         )
     elif data == "close":
