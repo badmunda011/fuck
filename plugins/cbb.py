0bc3ee4fbefc279fa9d7ef94f4b809ed4878aae4
SUKH ❤️ <158456267+PBSUKH@users.noreply.github.com>
2024-02-24 20:23:05 +0530

    Update cbb.py

--- plugins/cbb.py
+++ plugins/cbb.py
@@ -1,4 +1,4 @@
-# (©)Codexbotz
+
 
 from pyrogram import __version__
 from pyrogram.types import (
@@ -17,7 +17,7 @@ async def cb_handler(client: Bot, query: CallbackQuery):
     data = query.data
     if data == "about":
         await query.message.edit_text(
-            text=f"<b>○ Creator : <a href='tg://user?id={OWNER_ID}'>This Person</a>\n○ Language : <code>Python3</code>\n○ Library : <a href='https://docs.pyrogram.org/'>Pyrogram asyncio {__version__}</a>\n○ Source Code : <a href='https://github.com/CodeXBotz/File-Sharing-Bot'>Click here</a>\n○ Channel : @CodeXBotz\n○ Support Group : @CodeXBotzSupport</b>",
+            text=f"<b>○ Creator : <a href="<b>○ ᴄʀᴇᴀᴛᴏʀ : <a href='tg://user?id={OWNER_ID}'>🕊️⃝‌ٖٖٖٖ ‌ٖٖٖٖٖ𝐁α∂ ❤️ᥫ᭡፝֟፝֟</a>\n○ ʟᴀɴɢᴜᴀɢᴇ : <code>Python3</code>\n○ sᴏᴜʀᴄᴇ ᴄᴏᴅᴇ : <a href='https://github.com/Badhacker98?tab=repositories'>Click here</a>\n○ sᴜᴘᴘᴏʀᴛ : @ll_THE_BAD_BOT_ll\n○ <b>○ ᴏᴡɴᴇʀ: <a href='tg://user?id={OWNER_ID}'>This Person</a>\n",
             disable_web_page_preview=True,
             reply_markup=InlineKeyboardMarkup(
                 [[InlineKeyboardButton("❌ ᴄʟᴏsᴇ ❌", callback_data="close")]]
